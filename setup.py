5b8dfeb5c4def2d1fe6665185c216540fb3fdfb9
Chi-kwan Chan <chanc@arizona.edu>
2020-12-05 21:24:10 -0700

    Add a minimal `setup` script

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2020 Chi-kwan Chan
+# Copyright (C) 2020 Steward Observatory
+#
+# This file is part of PRay.
+#
+# PRay is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PRay is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PRay.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pray',
+    version='0.1.0',
+    url='https://github.com/luxsrc/pray',
+    author='Chi-kwan Chan',
+    author_email='chanc@arizona.edu',
+    description='General relativistic ray tracing in python based on JAX',
+    packages=find_packages('mod'),
+    package_dir={'': 'mod'},
+    python_requires='>=3.6', # `pray` uses python3's f-string and typing
+)
