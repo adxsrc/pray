bbb563acde8323e06a0507537282cf1a2720f171
Chi-kwan Chan <chanc@arizona.edu>
2020-12-06 00:51:20 -0700

    Make `PRay` the only available symbol from `pray`

--- mod/pray/__init__.py
+++ mod/pray/__init__.py
@@ -16,4 +16,4 @@
 # You should have received a copy of the GNU General Public License
 # along with PRay.  If not, see <http://www.gnu.org/licenses/>.
 
-from . import integrator
+from .core import PRay
